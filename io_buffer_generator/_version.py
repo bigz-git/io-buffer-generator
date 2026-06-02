c3c8890053299fb1b3ddc3e33d74e85ca56d84ca
bigz-git <zack.galdes@gmail.com>
2026-06-02 13:26:26 -0400

    change version to 1.5.3

--- io_buffer_generator/_version.py
+++ io_buffer_generator/_version.py
@@ -1,3 +1,6 @@
-__version__ = "1.5.3-beta.1"
-# beta.0 - code cleanup. No functional changes to tool
-# beta.1 - error check for open workbook
+__version__ = "1.5.3"
+# 1.5.3-beta.0 - code cleanup. No functional changes to tool
+# 1.5.3-beta.1 - error check for open workbook
+# 1.5.3 - stable release with the above changes.
+
+
